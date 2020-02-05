515a349615a9a3b11ac2bfc43515f14da9934933
Kirill Dubovitskiy <kirill2003de@gmail.com>
2020-02-04 19:51:02 -0800

    failing with error System.InvalidCastException: 'Specified cast is not valid.'

--- ViewWithInternalModelCrash/ViewWithInternalModelCrash.fs
+++ ViewWithInternalModelCrash/ViewWithInternalModelCrash.fs
@@ -8,24 +8,17 @@ open Fabulous.XamarinForms.LiveUpdate
 open Xamarin.Forms
 
 module App = 
-    type Model = unit
+    let init () = (), Cmd.none
 
-    type Msg = unit
+    let update (_: unit) _ = (), Cmd.none
 
-    let initModel = ()
-
-    let init () = initModel, Cmd.none
-
-    let update (msg: Msg) model = (), Cmd.none
-
-    let view (model: Model) dispatch =
+    let view (_: unit) _ =
         View.ContentPage(
             content = 
                 View.WithInternalModel(
                     init = fun () -> ()
-                    , update = fun () model -> model
-                    , view = fun model dispatch ->
-                        View.BoxView()
+                    , update = fun _ _ -> ()
+                    , view = fun _ _ -> View.BoxView()
                 )
         )
 
@@ -41,44 +34,3 @@ type App () as app =
         |> Program.withConsoleTrace
 #endif
         |> XamarinFormsProgram.run app
-
-#if DEBUG
-    // Uncomment this line to enable live update in debug mode. 
-    // See https://fsprojects.github.io/Fabulous/Fabulous.XamarinForms/tools.html#live-update for further  instructions.
-    //
-    //do runner.EnableLiveUpdate()
-#endif    
-
-    // Uncomment this code to save the application state to app.Properties using Newtonsoft.Json
-    // See https://fsprojects.github.io/Fabulous/Fabulous.XamarinForms/models.html#saving-application-state for further  instructions.
-#if APPSAVE
-    let modelId = "model"
-    override __.OnSleep() = 
-
-        let json = Newtonsoft.Json.JsonConvert.SerializeObject(runner.CurrentModel)
-        Console.WriteLine("OnSleep: saving model into app.Properties, json = {0}", json)
-
-        app.Properties.[modelId] <- json
-
-    override __.OnResume() = 
-        Console.WriteLine "OnResume: checking for model in app.Properties"
-        try 
-            match app.Properties.TryGetValue modelId with
-            | true, (:? string as json) -> 
-
-                Console.WriteLine("OnResume: restoring model from app.Properties, json = {0}", json)
-                let model = Newtonsoft.Json.JsonConvert.DeserializeObject<App.Model>(json)
-
-                Console.WriteLine("OnResume: restoring model from app.Properties, model = {0}", (sprintf "%0A" model))
-                runner.SetCurrentModel (model, Cmd.none)
-
-            | _ -> ()
-        with ex -> 
-            App.program.onError("Error while restoring model found in app.Properties", ex)
-
-    override this.OnStart() = 
-        Console.WriteLine "OnStart: using same logic as OnResume()"
-        this.OnResume()
-#endif
-
-
