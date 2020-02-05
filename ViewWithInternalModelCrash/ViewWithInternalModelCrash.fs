3f9ea1c6a607f515bdc5721cfa1213185d62a69e
Kirill Dubovitskiy <kirill2003de@gmail.com>
2020-02-04 19:43:01 -0800

    Add project files.

---
+++ ViewWithInternalModelCrash/ViewWithInternalModelCrash.fs
@@ -0,0 +1,84 @@
+﻿// Copyright 2018-2019 Fabulous contributors. See LICENSE.md for license.
+namespace ViewWithInternalModelCrash
+
+open System.Diagnostics
+open Fabulous
+open Fabulous.XamarinForms
+open Fabulous.XamarinForms.LiveUpdate
+open Xamarin.Forms
+
+module App = 
+    type Model = unit
+
+    type Msg = unit
+
+    let initModel = ()
+
+    let init () = initModel, Cmd.none
+
+    let update (msg: Msg) model = (), Cmd.none
+
+    let view (model: Model) dispatch =
+        View.ContentPage(
+            content = 
+                View.WithInternalModel(
+                    init = fun () -> ()
+                    , update = fun () model -> model
+                    , view = fun model dispatch ->
+                        View.BoxView()
+                )
+        )
+
+    // Note, this declaration is needed if you enable LiveUpdate
+    let program = Program.mkProgram init update view
+
+type App () as app = 
+    inherit Application ()
+
+    let runner = 
+        App.program
+#if DEBUG
+        |> Program.withConsoleTrace
+#endif
+        |> XamarinFormsProgram.run app
+
+#if DEBUG
+    // Uncomment this line to enable live update in debug mode. 
+    // See https://fsprojects.github.io/Fabulous/Fabulous.XamarinForms/tools.html#live-update for further  instructions.
+    //
+    //do runner.EnableLiveUpdate()
+#endif    
+
+    // Uncomment this code to save the application state to app.Properties using Newtonsoft.Json
+    // See https://fsprojects.github.io/Fabulous/Fabulous.XamarinForms/models.html#saving-application-state for further  instructions.
+#if APPSAVE
+    let modelId = "model"
+    override __.OnSleep() = 
+
+        let json = Newtonsoft.Json.JsonConvert.SerializeObject(runner.CurrentModel)
+        Console.WriteLine("OnSleep: saving model into app.Properties, json = {0}", json)
+
+        app.Properties.[modelId] <- json
+
+    override __.OnResume() = 
+        Console.WriteLine "OnResume: checking for model in app.Properties"
+        try 
+            match app.Properties.TryGetValue modelId with
+            | true, (:? string as json) -> 
+
+                Console.WriteLine("OnResume: restoring model from app.Properties, json = {0}", json)
+                let model = Newtonsoft.Json.JsonConvert.DeserializeObject<App.Model>(json)
+
+                Console.WriteLine("OnResume: restoring model from app.Properties, model = {0}", (sprintf "%0A" model))
+                runner.SetCurrentModel (model, Cmd.none)
+
+            | _ -> ()
+        with ex -> 
+            App.program.onError("Error while restoring model found in app.Properties", ex)
+
+    override this.OnStart() = 
+        Console.WriteLine "OnStart: using same logic as OnResume()"
+        this.OnResume()
+#endif
+
+
